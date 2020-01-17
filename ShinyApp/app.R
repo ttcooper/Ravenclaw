db657d8b4ff1ca724ceff4d68f8687e11c6f81f2
ttcooper <59985951+ttcooper@users.noreply.github.com>
2020-01-17 15:58:54 +0000

    Add files via upload

--- ShinyApp/app.R
+++ ShinyApp/app.R
@@ -22,9 +22,9 @@ ui <- fluidPage(
         sidebarPanel(
             selectInput(
                 inputId  = "check",
-                label = "Select the Machine Learning Technique",
-                choices = c("Linear Regression", "Random Forest", "Neural Network"),
-                selected = "Random Forest"
+                label = "Select the results you want to view",
+                choices = c("Results Summary", "Linear Regression Onshore", "Linear Regression Offshore", "Random Forest View 1", "Random Forest View 2", "Neural Network"),
+                selected = "Results Summary"
         )),
         
         # Show a plot of the generated distribution
@@ -38,14 +38,23 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
     output$image <- renderImage({
-        if (input$check == "Random Forest") {
+        if (input$check == "Random Forest View 1") {
             return(list(
-                src = "images/RF_results.png",
+                src = "images/RF_results_v1.png",
                 contentType = "png",
-                alt = "Random Forest",
-                height = 400, 
+                alt = "Random Forest View 1",
+                height = 300, 
                 width = 560
             ))
+        } else if (input$check == "Random Forest View 2") {
+             return(list(
+                src = "images/RF_results_v2.png",
+                contentType = "png",
+                alt = "Random Forest View 2",
+                height = 400, 
+                width = 600
+                ))
+
         } else if (input$check == "Neural Network") {
             return(list(
                 src = "images/NN_results.png",
@@ -54,13 +63,32 @@ server <- function(input, output) {
                 height = 400, 
                 width = 560
             ))
-        } else if (input$check == "Linear Regression") {
+            
+        } else if (input$check == "Linear Regression Onshore") {
             return(list(
-                src = "images/LR_results.png",
+                src = "images/LR_onshore_results.png",
                 contentType = "png",
-                alt = "Linear Regression",
-                height = 300, 
-                width = 560
+                alt = "Linear Regression Onshore",
+                height = 400, 
+                width = 600
+            ))
+            
+        } else if (input$check == "Linear Regression Offshore") {
+            return(list(
+                src = "images/LR_offshore_results.png",
+                contentType = "png",
+                alt = "Linear Regression Offshore",
+                height = 400, 
+                width = 600
+            ))
+            
+        } else if (input$check == "Results Summary") {
+            return(list(
+                src = "images/results_compare.png",
+                contentType = "png",
+                alt = "Results Summary",
+                height = 110, 
+                width = 420
             ))
         } 
     }, deleteFile = FALSE)
